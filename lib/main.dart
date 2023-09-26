c27093f4d7203594a8987e46aa8c5c4df6933c8a
Tristin Sorrells <Tristinsorrells1@gmail.com>
2023-09-26 12:30:59 -0500

    Basic styling and structure

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_app_gallery/network/endpoints.dart';
+import 'package:flutter_app_gallery/network/imageService.dart';
 import 'package:flutter_app_gallery/widgets/imageCard.dart';
+import 'package:flutter_app_gallery/models/webimage.dart';
+import 'package:http/http.dart' as http;
 
 void main() {
   runApp(const MyApp());
@@ -33,8 +36,41 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   EndPoints endPoints = EndPoints();
-  String authorName = "Author of Image";
+  List<WebImage> webImages = [];
 
+  @override
+  void initState() {
+    super.initState();
+    _fetchWebImages();
+  }
+
+  Future<void> _fetchWebImages() async {
+    try {
+      List<WebImage> images = await ImageWebService(http.Client()).fetchListOfWebImages();
+      setState(() {
+        webImages = images;
+      });
+    } catch (e) {
+      // Handle errors
+      print("Error fetching web images: $e");
+    }
+  }
+
+   PageController _pageController = PageController(initialPage: 0);
+
+  void _previousImage() {
+    _pageController.previousPage(
+      duration: Duration(milliseconds: 300),
+      curve: Curves.easeInOut,
+    );
+  }
+
+  void _nextImage() {
+    _pageController.nextPage(
+      duration: Duration(milliseconds: 300),
+      curve: Curves.easeInOut,
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -43,10 +79,55 @@ class _MyHomePageState extends State<MyHomePage> {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
       ),
-      body:  Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: ImageCard(authorName:authorName,imageUrl:endPoints.getImageSize(400,400)),
+      body: Column(
+        children: [
+          Expanded(
+            child: PageView.builder(
+              controller: _pageController,
+              itemCount: webImages.length,
+              itemBuilder: (context, index) {
+                return Center(
+                  child: SizedBox(
+                    height: 400,
+                    child: ImageCard(
+                      authorName: webImages[index].author,
+                      imageUrl: webImages[index].download_url,
+                    ),
+                  ),
+                );
+              },
+            ),
+          ),
+          Container(
+            color: Colors.grey.shade900,
+            padding: EdgeInsets.symmetric(horizontal: 90.0), // Adjust the padding here
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                IconButton(
+                  icon: Text(
+                    "<",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 40.0,
+                    ),
+                  ),
+                  onPressed: _previousImage,
+                ),
+                IconButton(
+                  icon: Text(
+                    ">",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 40.0,
+                    ),
+                  ),
+                  onPressed: _nextImage,
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
